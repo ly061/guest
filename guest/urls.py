0efe3b7b39e7035e33834e1867bd5654f7cca860
ly061 <yan.liu@mullenloweprofero.com>
2019-08-21 18:32:30 +0800

    company 02

--- guest/urls.py
+++ guest/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from sign import views
+
+from guest.sign import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
