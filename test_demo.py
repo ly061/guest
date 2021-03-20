b038589491fe6177596c6f9a97a46353bbde8df8
ly <ly@qq.com>
2021-03-20 16:16:10 +0800

    version 1

--- test_demo.py
+++ test_demo.py
@@ -1,13 +1,58 @@
-import pytest
-import allure
-import requests
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import random
+import json
+
+app = FastAPI()  # 必须实例化该类，启动的时候调用
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8080/"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@pytest.mark.demo
-@allure.feature("request demo feature")
-@pytest.mark.parametrize('status_code', [200, 300, 400])
-def test_demo1(status_code):
-    with allure.step("访问百度"):
-        r = requests.get("https://www.baidu.com")
-        assert r.status_code == status_code
+# 请求验证码
+@app.get('/captcha')
+def index():
+    s = random.randint(1, 99)
+    return {"data": {
+        'message': f'获取验证码成功！--{s}', 'code': 200
+    }}
 
+
+# post登录
+@app.post('/login')
+def login():
+    return {'message': '登录成功',
+            'code': 200,
+            "obj": {
+                "tokenHead": "Bearer",
+                "token": "adasda3wqwdqwdqwdqwd12q3wd1q21wd2q3wd1q2w1d"
+            }
+            }
+
+
+# 请求菜单
+@app.get('/system/config/menu')
+def get_menu():
+    with open('data.json', encoding='utf8') as f:
+        data = f.read()
+    data = json.loads(data)
+    return data
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, host="127.0.0.1", port=8088)
+    print(get_menu())
